0f4d8f64330627d7cc738c207a43829499f4c29d
Ivan Tiurikov <080399ivana@gmail.com>
2023-02-27 21:54:18 +0300

    ДЗ 2 опять

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// int Digit(int num)
+// {
+//     return num % 100 / 10;
+// }
+// int number = new Random().Next(100, 1000);
+// Console.WriteLine($"We get {number}, digit is {Digit(number)}");
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// void Check(int num)
+// {
+//     if(num < 100) Console.WriteLine("Not a three-digit number");
+//     else
+//     {
+//         while(num >= 999)
+//         {
+//             num = num / 10;
+//         }
+//     Console.WriteLine($"Digit is {num % 10}");
+//     }
+// }
+// Console.Write("Input the number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Check(number);
+
+
+
